04f8d9f395c94aa5de59ed15422c47c295fa961f
Swapnanil-git <swapsar02@gmail.com>
2025-08-31 01:54:14 +0530

    second commit

--- src/flattenObject.ts
+++ src/flattenObject.ts
@@ -1,12 +1,12 @@
-function flattenObject(obj:any, parentKey: string = '', result: any = {}): any {
-    for (let key in obj){
-        let newKey = parentKey ?parentKey + "." + key : key;
+export function flattenObject(obj: any, parentKey: string = '', result: any = {}): any {
+    for (let key in obj) {
+        let newKey = parentKey ? parentKey + "." + key : key;
         let value = obj[key];
         // check the type of the value - object and key not null - swp
-        if (typeof value === 'object' && value !== null){
+        if (typeof value === 'object' && value !== null) {
             //check if array - swp
-            if(Array.isArray(value)){
-                for(let i = 0; i<value.length; i++){  // iterate through array - swp
+            if (Array.isArray(value)) {
+                for (let i = 0; i < value.length; i++) {  // iterate through array - swp
                     flattenObject(value[i], newKey + "." + i, result);
                 }
             } else {
@@ -20,15 +20,14 @@ function flattenObject(obj:any, parentKey: string = '', result: any = {}): any {
 }
 
 
-const obj ={
-    a:1,
-    b:{
-        c:'hello',
-        d:{e:true},
+const obj = {
+    a: 1,
+    b: {
+        c: 'hello',
+        d: { e: true },
     },
-    f:[{g:"world"}],
+    f: [{ g: "world" }],
 };
 
 const flat = flattenObject(obj);
 console.log(flat);
-module.exports = { flattenObject };
